532a484037095c59439a9e522066c1199e77523b
Andrea <andreademarco91@gmail.com>
2020-03-25 17:14:39 +0100

    FAKER WORKS

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,15 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @dd($posts)
+    @foreach ($posts as $post)
+      <div class="box">
+        <h2>{{$post->title}} di {{$post->author}}</h2>
+        <p>{{$post->description}}</p>
+        <img src="{{$post->img}}" alt="">
+      </div>
+    @endforeach
   </body>
 </html>
