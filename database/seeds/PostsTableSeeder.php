67df8d37e470fdb433feeaf9f70f8e646629f353
Andrea <andreademarco91@gmail.com>
2020-03-25 16:55:12 +0100

    MIGRATION/MODEL/SEEDER OK

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+      for ($i=0; $i < 3; $i++) {
+        $post = new Post();
+        $post->title = $faker->realText($maxNbChars = 15, $indexSize = 1);
+        $post->author = $faker->name;
+        $post->description = $faker->realText($maxNbChars = 400, $indexSize = 2);
+        $post->img = $faker->imageUrl;
+
+        $post->save();
+      }
+    }
+}
